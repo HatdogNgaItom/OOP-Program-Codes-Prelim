a785be166c7e1d4c548a4ebe8036c47e69a86a7e
Admin5 <Admin5@SED>
2025-09-07 00:13:41 +0800

    moved to folder

---
+++ Yagonia_RangeLimitations/Yagonia_RangeLimitations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_RangeLimitations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jack Anthony D. Yagonia
+            //IT306
+            //Check if a number entered by the user is between a certain range of numbers (inclusive).
+
+            int firstNum = 30;
+            int secondNum = 50;
+
+        Main:
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("This system tells whether the number that the user has given is either withing  '\nthe range limit or beyond the range limit of 30 to 50.");
+            Console.Write("'\nProvide a certain number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number >= 30 && number <= 50)
+            {
+                Console.WriteLine("The number provided is inside the range limit.");
+            }
+
+            else
+            {
+                Console.WriteLine("The number provided is outside the range limit.");
+            }
+
+            Console.Write("\nDo you want to have another entry(y/n)?: ");
+            char ans = Convert.ToChar(Console.ReadLine());
+
+            if (ans == 'y')
+            {
+                goto Main;
+            }
+            else if (ans == 'n')
+            {
+                return;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Yagonia_RangeLimitations/Yagonia_RangeLimitations/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Yagonia_RangeLimitations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Yagonia_RangeLimitations")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55befb23-ff2f-44d4-8e35-5f280b7d61e0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
