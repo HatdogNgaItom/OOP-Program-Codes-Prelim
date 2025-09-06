98fa4bdedff48d8a8e176404a3a2ef1844e9aa0c
Admin5 <Admin5@SED>
2025-09-07 00:01:31 +0800

    Moved to folder

---
+++ Yagonia_Badminton(Constructors)/Yagonia_Badminton(Constructors)/Players.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_Badminton_Constructors_
+{
+    internal class Players
+    {
+        public string name;
+        public string from;
+        public string titles;
+
+        //Generic Method to Display
+        public void DisplayInfo()
+        {
+            name = "Lin Dan";
+            from = "China";
+            titles = "Olympic gold medals (2008, 2012), 5 World Championship titles\n6 All England Open titles";
+
+
+        }
+    }
+}
